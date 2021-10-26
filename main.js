555e80750a9ed8138f336d806a9bc02461c51bd7
Mynul Sakil <2052cse00832@manarat.ac.bd>
2021-10-26 21:03:55 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+
+function submitIssue(e) {
+  const getInputValue = id => document.getElementById(id).value;
+  const description = getInputValue('issueDescription');
+  const severity = getInputValue('issueSeverity');
+  const assignedTo = getInputValue('issueAssignedTo');
+  const id = Math.floor(Math.random() * 100000000) + '';
+  const status = 'Open';
+
+  const issue = { id, description, severity, assignedTo, status };
+  let issues = [];
+  if (localStorage.getItem('issues')) {
+    issues = JSON.parse(localStorage.getItem('issues'));
+  }
+  issues.push(issue);
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  document.getElementById('issueInputForm').reset();
+  fetchIssues();
+  e.preventDefault();
+}
+
+const closeIssue = id => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const currentIssue = issues.find(issue => issue.id == id);
+  // console.log(currentIssue)
+  currentIssue.status = 'Closed';
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
+}
+
+const deleteIssue = id => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  // console.log(issues.id)
+  const remainingIssues = issues.filter(issue => issue.id != id)
+  localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues();
+}
+
+const fetchIssues = () => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const issuesList = document.getElementById('issuesList');
+  issuesList.innerHTML = '';
+  // console.log(issues)
+  let totalIssue = issues.length;
+  let closedIssueCount = 0;
+
+  // if (issues.length>0){
+  for (var i = 0; i < issues.length; i++) {
+    let { id, description, severity, assignedTo, status } = issues[i];
+    //if status closed line though form description
+    if (status == 'Closed') {
+      description = `<del>${description}<del>`
+      closedIssueCount++;
+    }
+    issuesList.innerHTML += `<div class="well">
+                              <h6>Issue ID: ${id} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              </div>`;
+  }
+  const issueTrackerText = document.getElementById('issue-trackerText');
+ let currentUnclosedIssue=totalIssue-closedIssueCount;
+  issueTrackerText.innerHTML = `Issue Tracker: ${currentUnclosedIssue}(${totalIssue})`
+}
+// }
